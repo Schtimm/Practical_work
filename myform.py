6a35456ddd8ac73200b46a23d38260d379c4b621
user1 <user1@stdm2.local>
2023-04-26 12:14:31 +0300

    Сделал обработку вопроса и почты:

--- myform.py
+++ myform.py
@@ -10,14 +10,44 @@ def my_form():
     mail = request.forms.get('ADRESS')
     login=request.forms.get('Username')
     question=request.forms.get('QUEST')
-    pdb.set_trace()
+    #pdb.set_trace()
     data={}
     if(mail!="" and login!="" and question!=""):
         if(re.match(pattern, mail) is not None):
             #data[mail]=question
             data[mail]=[login, question]
-            with open('data.txt', 'a') as outfile:
-                json.dump(data, outfile)
+            with open('data.txt') as json_file:
+                    content = json_file.read()
+                    if(content!=""):
+                        content=json.loads(content)
+                        if mail not in content:
+                            json_file.close()
+                            with open('data.txt', 'a') as outfile:
+                                if(len(question)>3 and question.isdigit()==False):
+                                    json.dump(data, outfile)
+                                else:
+                                    return "The question must be longer then 3 symbols and not consist of only digits.\
+                                    access date: "+str(datetime.now().date())
+                        else:
+                            json_file.close()
+                            with open('data.txt') as json_file:
+                                data=json.load(json_file)
+                                if(question not in data[mail]):
+                                    data[mail].append(question)
+                                json_file.close()
+                                with open('data.txt', 'w') as json_file:
+                                    if(len(question)>3 and question.isdigit()==False):
+                                        json.dump(data, json_file)
+                                    else:
+                                        return "The question must be longer then 3 symbols and not consist of only digits.\
+                                        access date: "+str(datetime.now().date())
+                    else:
+                        with open('data.txt', 'a') as outfile:
+                            if(len(question)>3 and question.isdigit()==False):
+                                json.dump(data, outfile)
+                            else:
+                                return "The question must be longer then 3 symbols and not consist of only digits.\
+                                access date: "+str(datetime.now().date())
             return "Thanks, " +str(login)+"! The answer will be sent to the mail %s" % mail+"\
             access date: "+str(datetime.now().date())
         else:
